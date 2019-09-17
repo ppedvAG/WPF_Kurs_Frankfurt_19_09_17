28bc873537d7c5ac2e2ef1fd798ea4de89515c5e
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2019-09-17 16:01:21 +0200

    Add project files.

---
+++ EventRouting/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventRouting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SP_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TblOutput.Text += (sender as StackPanel).Name + " PreviewMouseDown\n";
+            if ((sender as StackPanel).Name == "Yellow") e.Handled = true;
+        }
+
+        private void SP_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TblOutput.Text += (sender as StackPanel).Name + " MouseDown\n";
+        }
+    }
+}
